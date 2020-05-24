c3e528c3bdf3990b0a3e6e42248bce931a727556
emicsto <emicsto@gmail.com>
2020-05-24 12:06:18 +0200

    Add cli menu

--- src/main/kotlin/algorithms_6/Algorithms.kt
+++ src/main/kotlin/algorithms_6/Algorithms.kt
@@ -1,19 +1,37 @@
 package algorithms_6
 
 import algorithms_6.algorithms.FibonacciAlgorithm
+import algorithms_6.algorithms.HanoiTowerAlgorithm
 import algorithms_6.algorithms.QuicksortAlgorithm
+import java.util.*
+import kotlin.system.exitProcess
 
 class Algorithms {
 
 }
 
 fun main() {
-    val fibonacci = FibonacciAlgorithm(10)
-    println("Iterative " + fibonacci.iterative())
-    println("Recursive " + fibonacci.recursive())
+    println("[1] Fibonacci sequence")
+    println("[2] Hanoi tower problem")
+    println("[3] Quicksort")
+    print("Choose algorithm: ")
 
-    val quicksort = QuicksortAlgorithm(10)
-    println("Iterative " + quicksort.iterative())
-    println("Recursive " + quicksort.recursive())
+    val reader = Scanner(System.`in`)
+    val chosenAlgorithm: Int = reader.nextInt()
+
+    val algorithm = when (chosenAlgorithm) {
+        1 -> FibonacciAlgorithm()
+        2 -> HanoiTowerAlgorithm()
+        3 -> QuicksortAlgorithm()
+        else -> exitProcess(1)
+    }
+
+    println("\nYou selected " + algorithm.name)
+    print(algorithm.instruction)
+    val n: Int = reader.nextInt()
+    algorithm.n = n
+
+    println("Iterative (execution time in nanoseconds): " + algorithm.iterative())
+    println("Recursion (execution time in nanoseconds): " + algorithm.recursive())
 }
 
